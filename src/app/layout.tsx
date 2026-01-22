74a301e593400d77993d83d24abbd3355db4ba6e
Prime Builder <builder@primeengine.ai>
2026-01-22 23:09:35 +0530

    feat: add google adsense script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import MouseMoveHandler from '@/components/MouseMoveHandler'
 import ScrollProgress from '@/components/ScrollProgress'
+import Script from 'next/script'
 
 const inter = Inter({
   subsets: ['latin'],
@@ -22,6 +23,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={inter.variable} suppressHydrationWarning>
+      <head>
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9536327688388078"
+          crossOrigin="anonymous"
+          strategy="afterInteractive"
+        />
+      </head>
       <body>
         <ScrollProgress />
         <MouseMoveHandler />
